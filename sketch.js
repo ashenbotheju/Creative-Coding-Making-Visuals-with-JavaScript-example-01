e9a7768b80647ba117efc66ebd80f42c1b42cccb
G.A.K.BOTHEJU <ashenkavindagakb@gmail.com>
2024-08-31 12:59:54 +0530

    agent object error fix

--- sketch.js
+++ sketch.js
@@ -9,12 +9,12 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const agentA = new Agent(800, 400, 10)   //create object
-    const agentB = new Agent(700, 500, 10)   //create object
+    const agentA = new Agent(800, 500);
+    const agentB = new Agent(700, 300);
 
     agentA.drow(context);
     agentB.drow(context);
-
+    
   };
 };
 
@@ -22,23 +22,22 @@ canvasSketch(sketch, settings);
 
 //using class for multiple output
 class Point{
-  constructor(x, y, radius){
+  constructor(x, y, radius) {
     this.x = x;
     this.y = y;
-    this.radius = radius;
   }
 }
 
-class Agent{
-  constructor(x, y) {
+class  Agent {
+  constructor(x, y){
     this.pos = new Point(x, y);
-    this.radius = radius;
-  };
+    this.radius = 10;
+  }
 
-  drow(context) {
+  drow(context){
     context.beginPath();
-    context.arc(pos.x, pos.y, this.radius, 0, Math.PI * 2);
+    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
     context.fillStyle = 'black';
     context.fill();
   }
-};
+}
