b124fefb7919d3b8c405e8fac06e2c0a46d73960
G.A.K.BOTHEJU <ashenkavindagakb@gmail.com>
2024-08-31 13:17:31 +0530

    create for loop for generate multiple circles using random

--- sketch.js
+++ sketch.js
@@ -1,19 +1,27 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 1080, 1080 ]  //change demension
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
+  const agent = [];
+
+  for(let i = 0; i < 100; i++){
+    const x = random.range(0, width);
+    const y = random.range(0, height);
+
+    agent.push(new Agent(x, y));
+  };
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const agentA = new Agent(800, 500);
-    const agentB = new Agent(700, 300);
-
-    agentA.drow(context);
-    agentB.drow(context);
+    agent.forEach(agent => {
+      agent.drow(context);
+    });
     
   };
 };
@@ -25,19 +33,19 @@ class Point{
   constructor(x, y, radius) {
     this.x = x;
     this.y = y;
-  }
-}
+  };
+};
 
 class  Agent {
   constructor(x, y){
     this.pos = new Point(x, y);
     this.radius = 10;
-  }
+  };
 
   drow(context){
     context.beginPath();
     context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
     context.fillStyle = 'black';
     context.fill();
-  }
-}
+  };
+};
